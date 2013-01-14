36147b83aa172da6f0d42c27e57a0a14bc5cda30
bilal <bilal_shhabbir111@hotmail.com>
2013-01-15 02:51:30 +0500

    by bilal

--- assets/www/fgtLogic.js
+++ assets/www/fgtLogic.js
@@ -7,6 +7,7 @@ var chat_count = 0;
 var sending_time = 0;
 var domainName = "http://morning-scrubland-4310.herokuapp.com";
 fbDomainName = domainName;
+var myData;
 
 
 function startProcessing(){
@@ -21,50 +22,66 @@ $("#facebook_connect").live("click", function(e){
 	authenticate();
 });
 
+
 function displayAllPosts(data){
+//	db.transaction(fillPostsTable, errorCB, successCB);
 	var mhtml = '';
 	for(var i=0; i<data.length; i++){
-		var poster_name = data[i].poster_name;
 		var post_text = data[0].text;
 		if(post_text==null||post_text==""||data[i].status_type=="approved_friend"){
 			post_text = data[i].story;
 		}
 		var shared_pic = data[i].picture_url;
 		var post_time = data[i].created_at;
-		var total_comments = data[i].total_comments;
-		var total_likes = data[i].total_likes;
 		var user_image = "https://graph.facebook.com/"+data[i].poster_fb_id+"/picture";
-		console.log("user_image-------------------------"+user_image);
+		
 		var formatedTime = getDate(post_time);
 	  	formatedTime = getChangedDateView(formatedTime);
 	   	formatedTime = formatedTime.split(",")[1];
 	   	post_time = formatedTime+", "+getTimeFormatted(getTime(post_time));
-	   
 	   	
-	   	mhtml+='<a href="#photodetail" data-transition="slide">';
-        mhtml+='<li class="news_feed_content_li">';
+	   	mhtml+='<a href="#" data-transition="slide">';
+        mhtml+='<li class="news_feed_content_li" id="'+data[i].id+'">';
         mhtml+='<div class="news_feed_content_seperator"></div>';
         mhtml+='<div class="news_feed_content_container">';
         mhtml+='<div class="news_feed_content_c1">';
 		mhtml+='<div class="news_feed_content_c1r1">';
 		mhtml+='<img src="'+user_image+'"></div></div>';
 		mhtml+='<div class="news_feed_content_c2">';
-		mhtml+='<div class="news_feed_content_c2r1">'+poster_name+'</div>';
+		mhtml+='<div class="news_feed_content_c2r1">'+data[i].poster_name+'</div>';
 		mhtml+='<div class="news_feed_content_c2r2">'+post_text+'</div>';
 		mhtml+='<div class="news_feed_content_c2r3">';
-		mhtml+='<img src="images/like_btn.png">'+total_likes+'<img src="images/msg_btn.png">'+total_comments+'</div></div>';
+		mhtml+='<img src="images/like_btn.png">'+data[i].total_likes+'<img src="images/msg_btn.png">'+data[i].total_comments+'</div></div>';
 		mhtml+='<div class="news_feed_content_c3">';
 		mhtml+='<div class="news_feed_content_c3r1">'+post_time+'</div>';
 		mhtml+='<div class="news_feed_content_c3r2">';
-		mhtml+='<img src="'+shared_pic+'"></div></div></div>';
-		console.log("shared_pic-------------------"+shared_pic);
+		if(shared_pic!=null){
+			mhtml+='<img src="'+shared_pic+'">';
+		}
+		mhtml+='</div></div></div>';
 		mhtml+='<div class="clr"></div></li></a>';
-		
 	}
 	$(".news_feed_content_ul").html(mhtml);
-//	var eventTime = getTimeFormatted(getTime(event_start_date)) + ' - ' + getTimeFormatted(getTime(event_end_date)) + "," + currDate;
 }
 
+$('.news_feed_content_li').live('click',function(e){
+	var post_id = $(this).attr('id');
+	var url = domainName+'/post_detail.json?facebook_id='+window.localStorage.getItem("facebook_id")+'&post_id='+post_id;
+	$.getJSON(url,function(data){
+		alert(JSON.stringify(data));
+	});
+});
+
+/*
+function tables(tx){
+    tx.executeSql('DROP TABLE IF EXISTS POSTS');
+//    tx.executeSql('DROP TABLE IF EXISTS FRIENDS');
+//    tx.executeSql('DROP TABLE IF EXISTS MESSAGES');
+    tx.executeSql('CREATE TABLE IF NOT EXISTS POSTS (id unique, post_from)');
+//    tx.executeSql('CREATE TABLE IF NOT EXISTS FRIENDS (id unique, facebook_id, name)');
+//    tx.executeSql('CREATE TABLE IF NOT EXISTS MESSAGES (id unique, time, message, group_id, sender_id, sender_name,flag)');
+}
+*/
 
 function getCurrentMonthName(){
 	var d = new Date();
